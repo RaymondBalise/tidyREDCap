3f1e4b411cd9c607655bd27ec9c1efbccdb676c6
gabrielodom <gabriel_odom@baylor.edu>
2019-12-30 11:00:10 -0500

    Closes #1

--- R/make_binary_word.R
+++ R/make_binary_word.R
@@ -1,18 +1,26 @@
 #' Convert a "choose all that apply" Question Into a Binary Word
 #' 
-#' @description This function takes a dataframe holding binary variables with 
-#'     values corresponding to a dummy-coded "choose all that apply" question.  
-#'     It can be used for any *binary word* problem.
+#' @description This function takes a data frame holding binary variables with 
+#'    values corresponding to a dummy-coded "choose all that apply" question.  
+#'    It can be used for any *binary word* problem.
 #'
-#' @param df A dataframe with the variables corresponding to binary indicators 
-#'     (the dummy coded variables) for a "choose all that apply" question.
+#' @param df A data frame with the variables corresponding to binary indicators 
+#'    (the dummy coded variables) for a "choose all that apply" question.
 #' @param yes_value A character string that corresponds to choosing "yes" 
-#'     in the df binary variables.  Defaults to the REDCap "Checked" option. 
+#'    in the binary variables of `df`. Defaults to the REDCap "Checked" option. 
 #' @param the_labels A character vector of single letters holding the letters 
-#'     used to make the binary word.  See the article/vignette called "Make 
-#'     Binary Word" for an example.
+#'    used to make the binary word. See the article/vignette called "Make 
+#'    Binary Word" for an example: .
 #'
-#' @return A character vector with length equal to the rows of `df`
+#' @return A character vector with length equal to the rows of `df`, including
+#'    one letter or underscore for each column of `df`. For instance, if `df`
+#'    has one column for each of the eight options, with a row containing the
+#'    values "Chips" (checked), "Yellow cheese" (unchecked), "Orange cheese"
+#'    (checked), "White cheese" (checked), "Meat" (checked), "Beans"
+#'    (unchecked), "Tomatoes" (unchecked) and "Peppers" (checked), then
+#'    the character string corresponding to that row will be `"a_cde__h"`. The
+#'    underscores represent that the options for "Yellow cheese", "Beans", and
+#'    "Tomatoes" were left unchecked.
 #' 
 #' @importFrom purrr map_chr
 #' @importFrom stringr str_replace_na
