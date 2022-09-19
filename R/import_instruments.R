2aed70aa05b3a2ab03e6f918942f6b7e14d34a6b
wfd14 <wfd14@miami.edu>
2022-09-19 16:15:31 -0400

    Moving redcapapi dependence to suggests and renaming function

--- R/import_instruments.R
+++ R/import_instruments.R
@@ -1,51 +1,89 @@
+
 #' import_instruments
 #' 
-#' @description This function takes the name of a connection to a REDCap
+#' @description This function takes the url and key for a REDCap
 #' project and returns a table for each instrument/form in the project. 
 #'
-#' @param connection The name of connection created by a call to the
-#'   `redcapAPI::redcapConnection()` function
+#' @param url The API URL for your the instance of REDCap
+#' @param token The API security token
 #' @param drop_blank Drop records that have no data. TRUE by default.
 #' @param envir The name of the environment where the tables should be saved.
 #'
 #' @return datasets, by default in the global environment
 #' 
 #' 
-#' @importFrom redcapAPI exportInstruments exportRecords
-#' @importFrom dplyr pull
+#' @importFrom REDCapR redcap_read
+#' @importFrom dplyr pull if_else
 #' @importFrom magrittr %>%
-#' @importFrom stringr str_which
+#' @importFrom stringr str_remove
 #' @importFrom tidyselect ends_with
+#' @importFrom labelVector set_label
 #' @export
 #'
 #' @examples
 #'\dontrun{
-#'   rcon <- redcapAPI::redcapConnection(
-#'     url <- "https://redcap.miami.edu/api/",
-#'     token = Sys.getenv("REDCAP_PLACEHOLDER5_KEY")
-#'   )
-#'   
-#'   import_instruments(rcon)
+#'  import_instruments(
+#'     "https://redcap.miami.edu/api/", 
+#'     Sys.getenv("test_API_key")
+#'  )
 #'}   
-import_instruments <- function(connection, drop_blank = TRUE, envir = .GlobalEnv) {
-   #browser()
+import_instruments <- function(url, token, drop_blank = TRUE, envir = .GlobalEnv) {
+  #browser()
   
-  if(class(connection) != "redcapApiConnection"){
-    stop("The `connection` argument must come from `redcapAPI::redcapConnection()`. See the `Using the imports_instruments() function` vignette.", call. = FALSE)
-  }
-
+  ds_instrument <- 
+    REDCapR::redcap_metadata_read(redcap_uri=url, token=token)$data
+  
   # Get names of instruments
-  instrument_name <- NULL
-  data_name <- redcapAPI::exportInstruments(connection) %>%
-    pull(instrument_name)
+  form_name <- NULL
+  
+  instrument_name <- ds_instrument |> 
+    pull(form_name) |> 
+    unique() 
+
   
   # do the api call
-  redcap <- redcapAPI::exportRecords(connection)
+  #redcap <- redcapAPI::exportRecords(connection)
+  
+  raw_labels <- 
+    REDCapR::redcap_read(
+      redcap_uri = url, 
+      token = token,
+      raw_or_label_headers = "label",
+      records = 1
+    )$data
+  
+  just_labels <- raw_labels
+
+  raw_redcapr <- 
+    REDCapR::redcap_read(
+      redcap_uri = url, 
+      token = token,
+    )$data
+  
+  just_data <- raw_redcapr
+  
+  just_data[] <- 
+    mapply(nm = names(just_data),
+           lab = relabel(names(just_labels)),
+           FUN = function(nm, lab) {
+             labelVector::set_label(just_data[[nm]], lab)
+           }, 
+           SIMPLIFY = FALSE
+    )
+  
+  redcap <- just_data
+  
+  
+  
+  
+  
   
   # get the index (end) of instruments
-  i <- redcap %>%
-    names() %>%
-    str_which("_complete")
+  i <- 
+    which(
+      str_remove(names(redcap), "_complete") %in% instrument_name
+    )
+  
   # add placeholder
   bigI <- c(0, i)
   nInstr_int <- length(bigI) - 1
@@ -57,7 +95,6 @@ import_instruments <- function(connection, drop_blank = TRUE, envir = .GlobalEnv
   } else {
     meta=1
   }
-  
   
   # Load all datasets to the global environment
   for(dataSet in seq_len(nInstr_int)) {
@@ -77,13 +114,50 @@ import_instruments <- function(connection, drop_blank = TRUE, envir = .GlobalEnv
     
     # The order of the names from exportInstruments() matches the order of the
     #   data sets from exportRecords()
-    assign(
-      data_name[dataSet],
-      processed_blank,
-      envir = envir
-    )
     
+    if(nrow(processed_blank > 0)){
+      assign(
+        instrument_name[dataSet],
+        processed_blank,
+        envir = envir
+      )
+    } else{
+      warning(paste("The", instrument_name[dataSet], "instrument/form has 0 records and will not be imported. \n" ), call.=FALSE)
+      # How to print warning about no records... how disruptive should this be?
+    }
   }
   
   invisible()
+}
+
+#' @title relabel
+#' 
+#' @description This is a function to change labels to match REDCapAPI. REDCapR
+#' labels "choose all that apply" variables with "(choice= thingy)" vs. 
+#' ": thingy" in REDCapAPI.
+#'
+#' @param x Character string variable holding label to be check for possible 
+#' labels that need fixing to match REDCapAPI's variable label convention.
+#'
+#' @importFrom stringr str_count str_sub str_extract
+#'
+#' @return vector text changed
+#' 
+#' @examples
+#'\dontrun{
+#' relabel("What ingredients do you currently crave? (choice=Chips)")
+#'}
+
+relabel <- function(x) {
+  # regular expression (Reg Ex) to get content inside () after choice=
+  re <- "\\(choice=([^()]+)\\)" 
+  if_else(
+    stringr::str_count(x, "\\(choice") == 0,
+    x,
+    paste0(
+      stringr::str_sub(x, 1, str_locate(x, "\\(choice")[, 1] - 2),
+      ": ",
+      gsub(re, "\\1", stringr::str_extract(x, re)) # content inside of Reg Ex
+    )
+  )
 }
